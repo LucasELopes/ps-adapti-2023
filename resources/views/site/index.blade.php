e1300be1dc6c3a12f6978e907998cacd5e82aec6
LucasELopes <lucaslopeslll412@gmail.com>
2023-11-22 05:23:48 -0300

    checkpoint

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,17 @@
-{{-- Front-end começa aqui! --}}
+@extends('site.layouts.corpo')
+
+@section('subtitulo', 'Home')
+
+@section('conteudo')
+    <header id="cabecalho">
+      <div>
+        <img src="{{asset('user/img/page_seo_web_website_content_browser_search_job_find_icon_230037.svg')}}" alt="logo da GetJob">
+        <p id="site-nome">GetJob</p>
+      </div>
+    </header>
+    <main id="conteudo-principal">
+      <div class="card-aluno" >
+          
+      </div>
+    </main>
+@endsection
---
+++ resources/views/site/layouts/corpo.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  {{-- css --}}
+  <link rel="stylesheet" href="{{asset('user/css/style.css')}}">
+  
+  {{-- Fonte(s) --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Changa+One&display=swap" rel="stylesheet">
+
+  {{-- Icone na guia --}}
+  <link rel="shortcut icon" href="{{asset('user/img/page_seo_web_website_content_browser_search_job_find_icon_230037.svg')}}" type="image/x-icon">
+  
+  <title>GetJob - @yield('subtitulo')</title>
+</head>
+<body>
+    @yield('conteudo')
+</body>
+</html>
